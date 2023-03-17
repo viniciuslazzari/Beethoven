709956eb646cc0799544c010a11591c849c8f51a
Vinicius <vinilazzari028@gmail.com>
2023-03-17 17:43:23 -0300

    ✨ Interface initial mock

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from kivy.app import App
-from src.interface import Interface
+from src.templates.interface import Interface
 
 class Beethoven():
     def main(self):
-        inter = Interface()
-        inter.run()
+        interface = Interface()
+        interface.run()
 
 if __name__ == "__main__":
     bee = Beethoven()
