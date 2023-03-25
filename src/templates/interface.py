a135b146b9ebde7c6b02b400d4cfff20bbca0959
Vinicius <vinilazzari028@gmail.com>
2023-03-24 22:22:31 -0300

    ♻️ Get input data from interface

--- src/templates/interface.py
+++ src/templates/interface.py
@@ -4,4 +4,9 @@ class Interface(App):
     pass
 
     def say_hello(self):
-        print("Hello world!")
+        instrument = self.root.ids.instrument.text
+        bpm = self.root.ids.bpm.text
+        text = self.root.ids.text.text
+        file_path = self.root.ids.file.selection[0]
+
+        print(instrument, bpm, text, file_path)
